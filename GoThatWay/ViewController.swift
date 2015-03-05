d19abb655470c5c9a85bbaf5fd674d8061291180
Linenfelser <andrewlinenfelser@q.com>
2015-03-05 12:27:25 -0700

    Added function to get the users current location -Andrew

--- GoThatWay/ViewController.swift
+++ GoThatWay/ViewController.swift
@@ -10,11 +10,22 @@
 
 import UIKit
 import CoreMotion
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        
+        
         // Do any additional setup after loading the view, typically from a nib. 
     /*
         let motionManager: CMMotionManager = CMMotionManager()
@@ -37,11 +48,43 @@ class ViewController: UIViewController {
     }
     })
     }*/
+        
     }
 
        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error) -> Void in
+            
+            if(error != nil){
+                println("ERROR:" + error.localizedDescription)
+                return
+            }
+            if(placemarks.count > 0){
+                let pm = placemarks[0] as CLPlacemark
+                self.displayLocationInfo(pm)
+            }else{
+                println("Error with data")
+            }
+            
+        })
+    }
+    func displayLocationInfo(placemark: CLPlacemark) {
+        //once we have location, stop getting location to save battery
+        self.locationManager.stopUpdatingLocation()
+        println(placemark.locality)
+        println(placemark.postalCode)
+        println(placemark.administrativeArea)
+        println(placemark.country)
+    }
+
+    
+        func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!){
+            println("Error" + error.localizedDescription)
+        
     }
 
     @IBOutlet weak var label1: UILabel!
