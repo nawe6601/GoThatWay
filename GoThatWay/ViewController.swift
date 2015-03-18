a5971fea0ab24142785f22c877875d4a86855cfb
Pair Programming <pairprogram@rgnt2-92-193-dhcp.int.colorado.edu>
2015-03-18 17:51:13 -0600

    Location simulation working - Jesse

--- GoThatWay/ViewController.swift
+++ GoThatWay/ViewController.swift
@@ -15,27 +15,22 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
-    //var timer = NSTimer()
-    
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Do any additional setup after loading the view, typically from a nib.
+        
         self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
+        if (CLLocationManager.locationServicesEnabled()) {
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager.startUpdatingLocation()
+        }
         // Start heading updates.
         if (CLLocationManager.headingAvailable()) {
             self.locationManager.headingFilter = 1;
             self.locationManager.startUpdatingHeading()
-            //self.locationManager.heading.didChange(changeKind: NSKeyValueChange, valuesAtIndexes: <#NSIndexSet#>, forKey: <#String#>)
-            
         }
-       //timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target:self, selector: Selector("updateArrow"), userInfo: self,repeats: true)
-        // Do any additional setup after loading the view, typically from a nib.
-
         
     }
 
@@ -45,16 +40,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     @IBOutlet weak var arr1: UIImageView!
     @IBOutlet weak var DirectionLabel: UILabel!
-    
-    
-    func updateArrow(){
-        if(CLLocationManager.headingAvailable()){
-        //arr1.transform=CGAffineTransformMakeRotation(-((CGFloat(self.locationManager.heading.trueHeading)/180.0)*3.14))
-        arr1.transform=CGAffineTransformMakeRotation(-((CGFloat(self.locationManager.heading.trueHeading)/180.0)*3.14))
-        DirectionLabel.text=self.locationManager.heading.description
-        arr1.layer.shouldRasterize = true
-        }
-    }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error) -> Void in
@@ -86,26 +71,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             println("Error" + error.localizedDescription)
         
     }
+    
     func locationManager(manager: CLLocationManager!, didUpdateHeading newHeading: CLHeading!) {
         arr1.transform=CGAffineTransformMakeRotation(-((CGFloat(newHeading.trueHeading)/180.0)*3.14))
         DirectionLabel.text=self.locationManager.heading.description
-        arr1.layer.shouldRasterize = true
+        
     }
     
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var slider1: UISlider!
     
     @IBAction func slid(sender: AnyObject) {
-        //timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target:self, selector: Selector("updateArrow"), userInfo: nil,repeats: true)
         label1.text = "\(round(slider1.value*20)) ft"
         arr1.transform = CGAffineTransformMakeRotation(CGFloat(slider1.value)*5)
-        //DirectionLabel.text=self.locationManager.heading.description
-        //arr1.transform=CGAffineTransformMakeRotation(-((CGFloat(self.locationManager.heading.trueHeading)/180.0)*3.14))
         arr1.layer.shouldRasterize = true
     }
-    
-    
-   
-
+  
 }
 
