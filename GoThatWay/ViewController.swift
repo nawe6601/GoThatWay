dc4f9707eec3ccc24bcaf37f7eea9cea9c0ef3bf
Pair Programming <pairprogram@engr2-2-93-218-dhcp.int.colorado.edu>
2015-04-03 12:46:06 -0600

    5 working tests, distance & crosshair on map view - Jesse

--- GoThatWay/ViewController.swift
+++ GoThatWay/ViewController.swift
@@ -11,10 +11,24 @@
 import UIKit
 import CoreMotion
 import CoreLocation
+import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+var dist1 = 0.0;
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    @IBOutlet weak var arr1: UIImageView!
+    @IBOutlet weak var DirectionLabel: UILabel!
     
+    @IBOutlet weak var loclabel: UILabel!
+    
+    @IBOutlet weak var maplabel: UILabel!
+
+    @IBOutlet weak var mymap: MKMapView!
     let locationManager = CLLocationManager()
+    var myloc = CLLocation()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,19 +46,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             self.locationManager.startUpdatingHeading()
         }
         
+        if(self.locationManager.location != nil && self.maplabel != nil){
+            let region = MKCoordinateRegionMakeWithDistance(self.locationManager.location.coordinate, 2000, 2000)
+            self.mymap.setRegion(region, animated: true)
+        }
+        
     }
 
        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBOutlet weak var arr1: UIImageView!
-    @IBOutlet weak var DirectionLabel: UILabel!
     
-    @IBOutlet weak var loclabel: UILabel!
+    
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error) -> Void in
-            
+
             if(error != nil){
                 println("ERROR:" + error.localizedDescription)
                 return
@@ -52,13 +69,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             if(placemarks.count > 0){
                 let pm = placemarks[0] as CLPlacemark
                 //self.displayLocationInfo(pm)
+                if(self.loclabel != nil){
+                    self.loclabel.text = "Altitude: \(round(manager.location.altitude*3.28))"
+                    self.label1.text = "\(round(dist1))"
+                }else{
+                    dist1 = self.domapstuff(manager.location)
+                    self.maplabel.text = "\(round((dist1*3.28)/52.8)/100.0) mi."
+                }
+                
             }else{
                 println("Error with data")
             }
             
         })
-        loclabel.text = "Altitude: \(round(manager.location.altitude*3.28))"
+        
     }
+    
+    func MapManager(manager: MKMapViewDelegate!, mapViewWillStartRenderingMap locations: [AnyObject]!){
+        if(self.locationManager.location != nil && self.maplabel != nil){
+            dist1 = self.domapstuff(self.locationManager.location)
+            self.maplabel.text = "\(round((dist1*3.28)/52.8)/100.0) mi."
+        }
+        
+    }
+    
     
     func displayLocationInfo(placemark: CLPlacemark) {
         //once we have location, stop getting location to save battery
@@ -71,13 +105,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!){
             println("Error" + error.localizedDescription)
-        
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateHeading newHeading: CLHeading!) {
+        
+        if(arr1 != nil){
         arr1.transform=CGAffineTransformMakeRotation(-((CGFloat(newHeading.trueHeading)/180.0)*3.14))
         DirectionLabel.text="Heading: \(round(self.locationManager.heading.trueHeading))"
+        }
         
+        if(self.locationManager.location != nil && self.maplabel != nil){
+            dist1 = self.domapstuff(manager.location)
+            self.maplabel.text = "\(round((dist1*3.28)/52.8)/100.0) mi."
+        }
+        
+    }
+    
+    
+    func number(x: NSInteger) -> NSInteger {
+        return x*x
     }
     
     @IBOutlet weak var label1: UILabel!
@@ -88,6 +134,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         arr1.transform = CGAffineTransformMakeRotation(CGFloat(slider1.value)*5)
         arr1.layer.shouldRasterize = true
     }
+
+    func domapstuff(myloc: CLLocation) -> Double {
+        var center = mymap.centerCoordinate as CLLocationCoordinate2D
+        var getLat: CLLocationDegrees = center.latitude
+        var getLon: CLLocationDegrees = center.longitude
+        var getMovedMapCenter: CLLocation =  CLLocation(latitude: getLat, longitude: getLon)
+        return myloc.distanceFromLocation(getMovedMapCenter) as Double
+    }
   
 }
 
+
+//class ViewController2: UIViewController, MKMapViewDelegate {
+//    
+//    @IBOutlet weak var mymap: MKMapView!
+//    let locationManager = CLLocationManager()
+//    
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//    func domapstuff(didupdatemap myloc: CLLocation) -> Double {
+//        var center = mymap.centerCoordinate as CLLocationCoordinate2D
+//        var getLat: CLLocationDegrees = center.latitude
+//        var getLon: CLLocationDegrees = center.longitude
+//        var getMovedMapCenter: CLLocation =  CLLocation(latitude: getLat, longitude: getLon)
+//        
+//        return myloc.distanceFromLocation(getMovedMapCenter) as Double
+//    }
+//}
