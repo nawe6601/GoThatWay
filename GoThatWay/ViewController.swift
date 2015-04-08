48db86918690d126acdab317bb1ff6f79685506f
Pair Programming <pairprogram@engr2-30-252-dhcp.int.colorado.edu>
2015-04-08 14:51:44 -0600

    Cleaned up code - Jesse

--- GoThatWay/ViewController.swift
+++ GoThatWay/ViewController.swift
@@ -6,8 +6,6 @@
 //  Copyright (c) 2015 JSAHN. All rights reserved.
 //
 
-
-
 import UIKit
 import CoreMotion
 import CoreLocation
@@ -19,13 +17,10 @@ var dir1 = 0.0
 var destlat = 0.0
 var destlng = 0.0
 
-
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     @IBOutlet weak var arr1: UIImageView!
     @IBOutlet weak var DirectionLabel: UILabel!
-    
     @IBOutlet weak var loclabel: UILabel!
-    
     @IBOutlet weak var maplabel: UILabel!
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var slider1: UISlider!
@@ -34,12 +29,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     let locationManager = CLLocationManager()
     var myloc = CLLocation()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         self.locationManager.delegate = self
         if (CLLocationManager.locationServicesEnabled()) {
             self.locationManager.requestWhenInUseAuthorization()
@@ -51,28 +43,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             self.locationManager.headingFilter = 1;
             self.locationManager.startUpdatingHeading()
         }
-        
         if(self.locationManager.location != nil && self.maplabel != nil){
             let region = MKCoordinateRegionMakeWithDistance(self.locationManager.location.coordinate, 2000, 2000)
             self.mymap.setRegion(region, animated: true)
         }
-        
         if(self.DirectionLabel != nil){
             self.DirectionLabel.hidden = true
             self.label1.hidden = true
         }
-        
     }
 
-       override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        
-        
         self.getdir(manager.location)
         self.getdist(manager.location)
         if(self.loclabel != nil){
@@ -85,42 +71,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             self.getdist(manager.location)
             self.maplabel.text = "\(round(dist1*3.28084)) ft."
         }
-        
-        
-//        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, error) -> Void in
-//
-//            if(error != nil){
-//                println("ERROR:" + error.localizedDescription)
-//                return
-//            }
-//            if(placemarks.count > 0){
-//                let pm = placemarks[0] as CLPlacemark
-//                //self.displayLocationInfo(pm)
-//            }else{
-//                println("Error with data")
-//            }
-//            
-//        })
-        
     }
-    
-//    func MapManager(manager: MKMapViewDelegate!, mapViewWillStartRenderingMap locations: [AnyObject]!){
-//        if(self.locationManager.location != nil && self.maplabel != nil){
-//            self.getdist(self.locationManager.location)
-//            self.maplabel.text = "\(round((dist1*3.28)/52.8)/100.0) mi."
-//        }
-//        
-//    }
-    
-    
-//    func displayLocationInfo(placemark: CLPlacemark) {
-//        //once we have location, stop getting location to save battery
-//        self.locationManager.stopUpdatingLocation()
-//        println(placemark.locality)
-//        println(placemark.postalCode)
-//        println(placemark.administrativeArea)
-//        println(placemark.country)
-//    }
 
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!){
             println("Error" + error.localizedDescription)
@@ -132,7 +83,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             self.DirectionLabel.hidden = false
             self.label1.hidden = false
         }
-        
         if(self.newarr != nil){
             if(destlat != 0){
                 var dirfin = newHeading.trueHeading + dir1
@@ -141,10 +91,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                 }
                 newarr.transform=CGAffineTransformMakeRotation(-(CGFloat(dirfin * (M_PI / 180.0))))
             }
-                        //arr1.transform=CGAffineTransformMakeRotation(-(CGFloat(newHeading.trueHeading * (M_PI / 180.0))))
-            //DirectionLabel.text="Heading: \(round(self.locationManager.heading.trueHeading))"
         }
-        
         if(self.locationManager.location != nil && self.maplabel != nil){
             getdest()
             getdir(manager.location)
@@ -156,18 +103,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             getdist(manager.location)
             self.label1.text = "Distance: \(round(dist1*3.28084)) ft."
             self.DirectionLabel.text = "Heading: \(round(dir1*100.0)/100.0) deg."
-            
         }
-        
-        
     }
-    
     
     func number(x: NSInteger) -> NSInteger {
         return x*x
     }
-    
-    
     
     @IBAction func slid(sender: AnyObject) {
         label1.text = "\(round(slider1.value*20.0)) ft"
@@ -189,10 +130,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     func getdir(myloc: CLLocation) {
         var lat1rad = myloc.coordinate.latitude * (M_PI/180.0)
         var lng1rad = myloc.coordinate.longitude * (M_PI/180.0)
-        
         var lat2rad = destlat * (M_PI/180.0)
         var lng2rad = destlng * (M_PI/180.0)
-        
         var dir0 = atan2(sin(lng1rad-lng2rad)*cos(lat2rad), cos(lat1rad)*sin(lat2rad)-sin(lat1rad)*cos(lat2rad)*cos(lng1rad-lng2rad))*(180.0/M_PI)
         if (dir0 < 0.0){
             dir0 = dir0 + 360.0
